2fe701caab4e8a483dc1de75084916eb45f2ad27
Saúl Yaguana <saulyaguanacruz@gmail.com>
2023-02-18 21:15:17 -0500

    Viendo variables con Java

---
+++ Varaiables.java
@@ -0,0 +1,15 @@
+public class Varaiables {
+    public static void main(String[] args) {
+        //Declarando variable
+        int speed;
+        //Asignando el valor a una variable
+        speed = 10;
+        System.out.println(speed);
+
+        //Declarando e inicializando
+        int salary = 300;
+        String employeName = "Edward";
+
+        System.out.println("Employe name: " + employeName + "; salary: " + salary);
+    }
+}
